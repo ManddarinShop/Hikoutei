cca649edaf24709ffe6bcb0e5a9ebe52579664bf
ManddarinShop <dudgh102416@gmail.com>
2026-07-28 00:40:11 +0900

    feat(gateway): connect operation client and sync gateway

--- src/adapter/apps-script-gateway/index.ts
+++ src/adapter/apps-script-gateway/index.ts
@@ -1,38 +1,78 @@
 /** Signed adapter tools for the registry-bound Apps Script sync gateway. */
 
 export {
-  SYNC_GATEWAY_PROTOCOL_VERSION,
+  APPS_SCRIPT_OPERATION_NAMES,
+} from "./constants.js";
+export type { AppsScriptOperationName } from "./constants.js";
+
+export {
   canonicalSyncJson,
   syncSha256Hex,
-  syncGatewaySigningInput,
-  signSyncGatewayEnvelope,
-  createSyncGatewayEnvelope,
 } from "./syncProtocol.js";
 export type {
   SyncJsonValue,
-  SyncGatewayOperation,
-  SyncGatewayEnvelope,
-  CreateSyncGatewayEnvelopeOptions,
 } from "./syncProtocol.js";
-export {
-  SYNC_GATEWAY_ADMIN_PROTOCOL_VERSION,
-  syncGatewayAdminSigningInput,
-  signSyncGatewayAdminEnvelope,
-  createSyncGatewayAdminEnvelope,
-} from "./syncAdminProtocol.js";
-export type {
-  SyncGatewayAdminOperation,
-  SyncGatewayAdminEnvelope,
-  CreateSyncGatewayAdminEnvelopeOptions,
-} from "./syncAdminProtocol.js";
-export { AppsScriptSyncGatewayClient } from "./syncClient.js";
 export {
   AppsScriptSyncGatewayError,
   SYNC_GATEWAY_CLIENT_ERROR_CODES,
 } from "./errors.js";
-export type {
-  AppsScriptSyncGatewayClientOptions,
-  SyncGatewayProvisionRegistration,
-  SyncGatewayProvisionResult,
-} from "./syncClient.js";
 export type { SyncGatewayClientErrorCode } from "./errors.js";
+export {
+  APPS_SCRIPT_OPERATION_PROTOCOL_VERSION,
+  appsScriptOperationSigningInput,
+  signAppsScriptOperationEnvelope,
+  createAppsScriptOperationEnvelope,
+} from "./codeGsProtocol.js";
+export type {
+  AppsScriptOperationWire,
+  AppsScriptOperationPayload,
+  AppsScriptOperationSigningFields,
+  AppsScriptOperationEnvelope,
+  CreateAppsScriptOperationEnvelopeOptions,
+} from "./codeGsProtocol.js";
+export { AppsScriptOperationClient } from "./operationClient.js";
+export { createFastAppendRowsOperation } from "./fastAppendOperation.js";
+export { createReadTableRowsOperation } from "./tableReadOperation.js";
+export {
+  createApplyEffectsOperation,
+  createReadEffectPostconditionOperation,
+  createReadEffectPostconditionsOperation,
+} from "./effectOperation.js";
+export {
+  createEnsureRowAnchorsOperation,
+  createObserveSnapshotOperation,
+  createReadSnapshotOperation,
+} from "./observationOperation.js";
+export { AppsScriptOperationSyncGateway } from "./operationSyncGateway.js";
+export type {
+  AppsScriptOperationDefinition,
+  AnyAppsScriptOperationDefinition,
+  AppsScriptOperationGateway,
+  AppsScriptOperationResult,
+  AppsScriptOperationResults,
+  AppsScriptOperationClientOptions,
+  AppsScriptOperationRequestEvent,
+} from "./operationClient.js";
+export type {
+  AppsScriptFastAppendOperationArgs,
+  AppsScriptFastAppendOperationRequest,
+  AppsScriptFastAppendOperation,
+} from "./fastAppendOperation.js";
+export type {
+  AppsScriptApplyEffectsOperationArgs,
+  AppsScriptApplyEffectsOperationRequest,
+  AppsScriptReadEffectPostconditionOperationArgs,
+  AppsScriptReadEffectPostconditionsOperationArgs,
+} from "./effectOperation.js";
+export type {
+  AppsScriptEnsureRowAnchorsOperationArgs,
+  AppsScriptObserveSnapshotOperationArgs,
+  AppsScriptReadSnapshotOperationArgs,
+} from "./observationOperation.js";
+export type {
+  AppsScriptReadTableRowsRequest,
+} from "./tableReadOperation.js";
+export type {
+  AppsScriptOperationProjectionStatus,
+  AppsScriptOperationSyncGatewayOptions,
+} from "./operationSyncGateway.js";
